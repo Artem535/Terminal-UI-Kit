85b0c52b05c82e4e94b591e05c00e4c08834c4a1
Artem Durynin <artem5542@gmail.com>
2026-07-22 15:04:05 +0300

    feat: add render_styled_text to convert StyledText to FTXUI Element

--- tests/terminal_ui_kit/rendering/style_bridge_test.cc
+++ tests/terminal_ui_kit/rendering/style_bridge_test.cc
@@ -1,5 +1,6 @@
 #include "terminal_ui_kit/components/style_bridge.h"
 
+#include "terminal_ui_kit/core/styled_text.h"
 #include "terminal_ui_kit/testing/virtual_screen.h"
 #include <gtest/gtest.h>
 
@@ -56,6 +57,41 @@ TEST(StyleBridge, DefaultStyleAppliesNoDecoration) {
   EXPECT_FALSE(pixel.strikethrough);
   EXPECT_EQ(pixel.foreground_color, ftxui::Color::Default);
   EXPECT_EQ(pixel.background_color, ftxui::Color::Default);
+}
+
+TEST(StyleBridge, RenderStyledTextEmptyReturnsEmpty) {
+  StyledText text;
+  ftxui::Element elem = render_styled_text(text);
+  ftxui::Screen screen = test_support::render_to_screen(elem, 10, 1);
+  EXPECT_EQ(screen.ToString(), "          ");
+}
+
+TEST(StyleBridge, RenderStyledTextBoldSpan) {
+  StyledText text;
+  TextStyle bold_style;
+  bold_style.bold = true;
+  text.append(TextSpan{"hello", bold_style});
+  ftxui::Element elem = render_styled_text(text);
+  ftxui::Screen screen = test_support::render_to_screen(elem, 10, 1);
+  EXPECT_TRUE(screen.PixelAt(0, 0).bold);
+  EXPECT_EQ(screen.PixelAt(0, 0).character, "h");
+}
+
+TEST(StyleBridge, RenderStyledTextMultipleSpans) {
+  StyledText text;
+  TextStyle bold_style;
+  bold_style.bold = true;
+  text.append(TextSpan{"ab", bold_style});
+  text.append(TextSpan{"cd", TextStyle{}});
+  ftxui::Screen screen = test_support::render_to_screen(render_styled_text(text), 10, 1);
+  EXPECT_EQ(screen.PixelAt(0, 0).character, "a");
+  EXPECT_EQ(screen.PixelAt(0, 0).bold, true);
+  EXPECT_EQ(screen.PixelAt(1, 0).character, "b");
+  EXPECT_EQ(screen.PixelAt(1, 0).bold, true);
+  EXPECT_EQ(screen.PixelAt(2, 0).character, "c");
+  EXPECT_EQ(screen.PixelAt(2, 0).bold, false);
+  EXPECT_EQ(screen.PixelAt(3, 0).character, "d");
+  EXPECT_EQ(screen.PixelAt(3, 0).bold, false);
 }
 
 }  // namespace
